ae7397b37b50655521d7aff5ca48f2c695f289d3
jeferson <jefersonqueiroga@gmail.com>
2019-03-15 19:23:57 +0000

    Aplicação

--- AulaDeDSC/src/br/com/ifrn/teste/Print.java
+++ AulaDeDSC/src/br/com/ifrn/teste/Print.java
@@ -3,7 +3,9 @@ package br.com.ifrn.teste;
 public class Print {
 	
 	public static void main(String[] args) {
-		System.out.print("\nTeste de criação de Projeto Git");
+		System.out.print("\nTeste de criaÃ§Ã£o de Projeto Git");
+		System.out.print("Coloque outro print aqui abaixo:");
+		
 	}
 
 }
