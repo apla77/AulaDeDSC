8d7b0eaf17c5fb0542162bd49a9c54ebc6d14349
jeferson <jefersonqueiroga@gmail.com>
2019-03-15 19:25:01 +0000

    teste

--- AulaDeDSC/src/br/com/ifrn/teste/Print.java
+++ AulaDeDSC/src/br/com/ifrn/teste/Print.java
@@ -5,7 +5,7 @@ public class Print {
 	public static void main(String[] args) {
 		System.out.print("\nTeste de criação de Projeto Git");
 		System.out.print("Coloque outro print aqui abaixo:");
-		
+		System.out.print("Coloque outro print aqui abaixo:");
 	}
 
 }
