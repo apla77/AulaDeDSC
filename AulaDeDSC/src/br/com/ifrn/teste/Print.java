6bee6ec17e8f73df89b3a85e004ebc1ad5b9d246
apla77 <verocidade77@gmail.com>
2019-03-15 19:26:14 -0300

    segundo commit mauricio

--- AulaDeDSC/src/br/com/ifrn/teste/Print.java
+++ AulaDeDSC/src/br/com/ifrn/teste/Print.java
@@ -5,5 +5,8 @@ public class Print {
 	public static void main(String[] args) {
 		System.out.print("\nTeste de criação de Projeto Git");
 	}
-
+	
+	public static void metodoTeste(){ 
+		System.out.println("cheguei");
+	}
 }
